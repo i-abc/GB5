3c4322f5ac8fe366d9a6ed3f73a2d9a15ea31b40
i-abc <104631897+i-abc@users.noreply.github.com>
2023-08-04 22:13:16 +0800

    (重构|新功能 脚本): 新增 ARM，提供对比链接

--- gb5-test.sh
+++ gb5-test.sh
@@ -7,237 +7,320 @@
 # 人性化的交互，操作无门槛
 # 测试结果详细、易于分享
 
-# 配色
+##### 自定义常量 ######
+
+# 脚本发布版本
+script_version="v2023-08-04"
+
+# geekbench5发布版本
+geekbench_version="5.5.1"
+
+# geekbench5官方SHA-256
+geekbench_x86_64_official_sha256="32037e55c3dc8f360fe16b7fbb188d31387ea75980e48d8cf028330e3239c404"
+geekbench_aarch64_official_sha256="9eb3ca9ec32abf0ebe1c64002b19108bfea53c411c6b556b0c2689514b8cbd6f"
+
+# 下载源
+url_1="https://cdn.geekbench.com"
+url_2="https://ghproxy.com/https://github.com/i-abc/GB5/releases/download/Geekbench-${geekbench_version}"
+
+##### 配色 #####
+
 _red() {
-    echo -e "\033[0;31;31m$1\033[0m"
+    echo -e "\033{0;31;31m${1}\033[0m"
 }
 
 _yellow() {
-    echo -e "\033[0;31;33m$1\033[0m"
+    echo -e "\033[0;31;33m${1}\033[0m"
 }
 
 _blue() {
-    echo -e "\033[0;31;36m$1\033[0m"
+    echo -e "\033[0;31;36m${1}\033[0m"
 }
 
-clear
+##### 横幅 #####
+_banner() {
+    echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
+    echo -e '#            专用于服务器的GB5测试             #'
+    echo -e '#                 '$script_version'                  #'
+    echo -e '#        https://github.com/i-abc/gb5          #'
+    echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
+    echo
+}
 
-GB5_version="v2023-08-04"
-
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo -e '#            专用于服务器的GB5测试             #'
-echo -e '#                 '$GB5_version'                  #'
-echo -e '#        https://github.com/i-abc/gb5          #'
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo
-
-# 检测curl、fallocate、tar是否安装
-_yellow "检测所需软件包是否安装\n"
-if ! (command -v curl && command -v fallocate && command -v tar)
-then
-    # 安装curl、fallocate、tar
-    if command -v dnf
-    then
-        dnf -y install curl util-linux tar
-    elif command -v yum
-    then
-        yum -y install curl util-linux tar
-    elif command -v apt
-    then
-        apt -y install curl util-linux tar
+##### 检测某软件包是否安装，没安则自动安上，目前只支持RedHat、Debian系 #####
+_package() {
+    _yellow "正在检测所需的${1}是否安装"
+    # 检测软件包是否安装
+    if ! command -v "$1"; then
+        # 确认包管理器并安装软件包
+        if command -v dnf; then
+            dnf -y install "$2"
+        elif command -v yum; then
+            yum -y install "$2"
+        elif command -v apt; then
+            apt -y install "$2"
+        else
+            _blue "本机非RedHat、Debian系，暂不支持自动安装所需的软件包"
+            exit
+        fi
+        # 再次检测软件包是否安装
+        if ! command -v "$1"; then
+            _red "自动安装所需的${1}失败"
+            echo "请手动安装${1}后再执行本脚本"
+            exit
+        fi
     fi
-    # 再次检测curl、fallocate、tar是否安装成功
-    if ! (command -v curl && command -v fallocate && command -v tar)
-    then
-        _red "自动安装curl、fallocate、tar失败"
-        echo "请手动安装curl、fallocate、tar后再执行该脚本"
+}
+
+##### 确认架构及对应的tar包 #####
+# Geekbench 5、6只支持64位，即x86_64、aarch64
+_architecture() {
+    # 非64位直接退出
+    if [ "$(getconf LONG_BIT)" != "64" ]; then
+        echo "本脚本目前只支持64位处理器测试"
         exit
     fi
-fi
+
+    # 判断是x86_64还是aarch64
+    if [ "$(uname -m)" == "x86_64" ]; then
+        _blue "经检测，本机架构为：x86_64"
+        geekbench_tar_name=Geekbench-${geekbench_version}-Linux.tar.gz
+        geekbench_tar_folder=Geekbench-${geekbench_version}-Linux
+        geekbench_official_sha256=$geekbench_x86_64_official_sha256
+        geekbench_software_name=geekbench_x86_64
+    elif [ "$(uname -m)" == "aarch64" ]; then
+        _blue "经检测，本机架构为：aarch64"
+        geekbench_tar_name=Geekbench-${geekbench_version}-LinuxARMPreview.tar.gz
+        geekbench_tar_folder=Geekbench-${geekbench_version}-LinuxARMPreview
+        geekbench_official_sha256=$geekbench_aarch64_official_sha256
+        geekbench_software_name=geekbench5
+    else
+        echo "本脚本目前只支持x86_64、aarch64架构"
+        exit
+    fi
+}
+
+##### 创建目录 #####
+_mkdir() {
+    # 删除可能存在的残余文件
+    rm -rf ./GB5-i-abc
+
+    # 创建目录
+    mkdir ./GB5-i-abc
+}
+
+##### 检测内存，增加Swap #####
+_swap() {
+    # 检测内存
+    mem=$(free -m | awk '/Mem/{print $2}')
+    old_swap=$(free -m | awk '/Swap/{print $2}')
+    old_ms=$((mem+old_swap))
+    _blue "本机内存为：${mem}Mi"
+    _blue "本机Swap为：${old_swap}Mi"
+    _blue "本机内存加Swap总计：${old_ms}Mi\n"
+
+    # 判断内存+Swap是否小于1G
+    if [ "$old_ms" -ge 1024 ]; then
+        _yellow "经判断，本机内存加Swap和大于1G，满足GB5测试条件，测试开始。\n"
+    else
+        echo "经判断，本机内存加Swap和小于1G，不满足GB5测试条件，有如下解决方案："
+        echo "1. 添加Swap (该操作脚本自动完成，且在GB5测试结束后会把本机恢复原样)"
+        echo -e "2. 退出测试\n"
+        _yellow "请输入您的选择 (序号)：\c"
+        read -r choice_1
+        echo -e "\033[0m"
+        case "$choice_1" in
+            2)
+                rm -rf ./GB5-i-abc
+                exit;;
+            # 添加Swap
+            1)
+                _yellow "添加Swap任务开始，完成时间取决于硬盘速度，请耐心等候\n"
+                need_swap=$((1100-old_ms))
+                fallocate -l "$need_swap"M ./GB5-i-abc/swap
+                chmod 0600 ./GB5-i-abc/swap
+                mkswap ./GB5-i-abc/swap
+                swapon ./GB5-i-abc/swap
+
+                # 再次判断内存+Swap是否小于1G
+                new_swap=$(free -m | awk '/Swap/{print $2}')
+                new_ms=$((mem+new_swap))
+                if [ "$new_ms" -ge 1024 ]; then
+                    echo
+                    _blue "经判断，现在内存加Swap和为${new_ms}Mi，满足GB5测试条件，测试开始。\n"
+                else
+                    echo
+                    echo "很抱歉，由于未知原因，Swap未能成功新增，现在内存加Swap和为${new_ms}Mi，仍不满足GB5测试条件，有如下备选方案："
+                    echo "1. 强制执行GB5测试"
+                    echo -e "2. 退出测试\n"
+                    _yellow "请输入您的选择 (序号)：\c"
+                    read -r choice_2
+                    echo -e "\033[0m"
+                    case "$choice_2" in
+                        2)
+                            swapoff ./GB5-i-abc/swap
+                            rm -rf ./GB5-i-abc
+                            exit;;
+                        1)
+                            echo ;;
+                        *)
+                            rm -rf ./GB5-i-abc
+                            _red "输入错误，请重新执行脚本"
+                            exit;;
+                    esac
+                fi;;
+            *)
+                rm -rf ./GB5-i-abc
+                _red "输入错误，请重新执行脚本"
+                exit;;
+        esac
+    fi
+}
+
+##### 判断IP所在地，选择相应下载源 #####
+_judge_ip() {
+    country=$(curl ipinfo.io/country 2> /dev/null)
+    if [ -z "$country" ] || echo "$country" | grep "{"; then
+        echo "使用镜像源"
+        geekbench_tar_url=${url_2}/${geekbench_tar_name}
+    elif [ "$country" != "CN" ]; then
+        echo "使用默认源"
+        geekbench_tar_url=${url_1}/${geekbench_tar_name}
+    else
+        echo "使用镜像源"
+        geekbench_tar_url=${url_2}/${geekbench_tar_name}
+    fi
+}
+
+##### 下载Geekbench tar包 ######
+_download_geekbench() {
+    _yellow "测试软件下载中"
+    curl --progress-bar -o "./GB5-i-abc/${geekbench_tar_name}" "$geekbench_tar_url"
+}
+
+##### 计算SHA-256并比对 #####
+_sha256() {
+    # 计算SHA-256
+    geekbench_download_sha256=$(sha256sum ./GB5-i-abc/${geekbench_tar_name} | awk '{print $1}')
+    # 比对SHA-256
+    if [ "$geekbench_download_sha256" == "$geekbench_official_sha256" ]; then
+        _blue "经比对，下载的程序与官网SHA-256相同，放心使用\n"
+    else
+        _red "经比对，下载的程序与官网SHA-256不相同，退出脚本执行"
+        _red "事关重大，方便的话麻烦到 https://github.com/i-abc/gb5 提一个issue"
+        exit
+    fi
+}
+
+##### 解tar包 #####
+_tar() {
+    tar -xf ./GB5-i-abc/${geekbench_tar_name} -C ./GB5-i-abc
+}
+
+##### 运行测试 #####
+_run() {
+    _yellow "测试中\n"
+
+    # 计时开始
+    run_start_time=$(date +"%s")
+
+    ./GB5-i-abc/${geekbench_tar_folder}/${geekbench_software_name} | \
+        tee >(awk '/System Information/,/Uploading results to the Geekbench Browser/ {if ($0 ~ /Uploading results to the Geekbench Browser/) exit; print}') > ./GB5-i-abc/result.txt
+
+    # 计时结束
+    run_end_time=$(date +"%s")
+
+    # 计算测试运行时间
+    run_time=$((run_end_time - run_start_time))
+    run_time_minutes=$((run_time / 60))
+    run_time_seconds=$((run_time % 60 ))
+}
+
+##### 下载含测试结果的html ######
+_download_result_html() {
+    result_html_url=$(grep -E "https.*cpu\/[0-9]*$" ./GB5-i-abc/result.txt)
+
+    if wget --spider $result_html_url 2> /dev/null; then
+        wget -O ./GB5-i-abc/result.html $result_html_url 2> /dev/null
+    else
+        wget --no-check-certificate -O ./GB5-i-abc/result.html $result_html_url 2> /dev/null
+    fi
+}
+
+##### 输出结果 (含时间、参数、分数、链接) #####
+_output() {
+    # 时间
+    echo "当前时间: $(date +"%Y-%m-%d %H:%M:%S %Z")"
+    echo -e "净测试时长: $run_time_minutes分$run_time_seconds秒\n"
+
+    # 参数
+    _yellow "Geekbench 5 测试结果\n"
+    awk '/System Information/,/Memory Information/ {if ($0 ~ /Memory Information/) exit;
+    {sub("System Information", "系统信息"); sub("Processor Information", "处理器信息"); sub("Memory Information", "内存信息")}; print}' ./GB5-i-abc/result.txt
+
+    # 分数
+    awk -F'>' '/<div class='"'"'score'"'"'>/{print $2}' ./GB5-i-abc/result.html | \
+        awk -F'<' '{if (NR==1) {print "单核测试分数: "$1} else {print "多核测试分数: "$1}}'
+
+    # 链接
+    awk '/https.*cpu\/[0-9]*$/{print "详细结果链接: " $1}' ./GB5-i-abc/result.txt
+    cpu=$(awk -F 'with an | processor' '/Benchmark results for/{gsub(/ /,"%20",$2); print $2}' ./GB5-i-abc/result.html)
+    echo "可参考链接: https://browser.geekbench.com/search?k=v5_cpu&q=$cpu"
+
+    echo
+    awk '/https.*key=[0-9]*$/{print "个人保存链接: " $1}' ./GB5-i-abc/result.txt
+}
+
+##### 删除残余文件 #####
+_delete() {
+    swapoff ./GB5-i-abc/swap &> /dev/null
+    rm -rf ./GB5-i-abc
+    _yellow "残余文件清除成功"
+}
+
+
+##### main #####
+clear
+_banner
+
+_package wget wget
+_package tar tar
+_package fallocate util-linux
 
 clear
+_banner
 
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo -e '#            专用于服务器的GB5测试             #'
-echo -e '#                 '$GB5_version'                  #'
-echo -e '#        https://github.com/i-abc/gb5          #'
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo
+_architecture
 
-# 源
-url_1="https://cdn.geekbench.com/Geekbench-5.5.1-Linux.tar.gz"
-url_2="https://ghproxy.com/https://raw.githubusercontent.com/i-abc/GB5/main/Geekbench-5/Geekbench-5.5.1-Linux.tar.gz"
-
-# 删除可能存在的残余文件
-rm -rf ./GB5-test-32037e55c3
-
-# 创建工作目录
-mkdir ./GB5-test-32037e55c3
-
-# 检测内存
-mem=$(free -m | awk '/Mem/{print $2}')
-old_swap=$(free -m | awk '/Swap/{print $2}')
-old_ms=$((mem+old_swap))
-_blue "本机内存为：${mem}Mi"
-_blue "本机Swap为：${old_swap}Mi"
-_blue "本机内存加Swap总计：${old_ms}Mi\n"
-
-# 判断内存+Swap是否小于1G
-if [ "$old_ms" -ge 1024 ]
-then
-    _yellow "经判断，本机内存加Swap和大于1G，满足GB5测试条件，测试开始。\n"
-else
-    echo "经判断，本机内存加Swap和小于1G，不满足GB5测试条件，有如下解决方案："
-    echo "1. 添加Swap (该操作脚本自动完成，且在GB5测试结束后会把本机恢复原样)"
-    echo -e "2. 退出测试\n"
-    _yellow "请输入您的选择 (序号)：\c"
-    # 添加Swap
-    read -r choice_1
-    echo -e "\033[0m"
-    case "$choice_1" in
-        2)
-            rm -rf ./GB5-test-32037e55c3
-            exit;;
-        1)
-            _yellow "添加Swap任务开始，完成时间取决于硬盘速度，请耐心等候\n"
-            need_swap=$((1100-old_ms))
-#             dd if=/dev/zero of=./GB5-test-32037e55c3/dd bs=1M count="$need_swap"
-            fallocate -l "$need_swap"M ./GB5-test-32037e55c3/dd
-            chmod 600 ./GB5-test-32037e55c3/dd
-            mkswap ./GB5-test-32037e55c3/dd
-            swapon ./GB5-test-32037e55c3/dd
-            # 再次判断内存+Swap是否小于1G
-            new_swap=$(free -m | awk '/Swap/{print $2}')
-            new_ms=$((mem+new_swap))
-            if [ "$new_ms" -ge 1024 ]
-            then
-                echo
-                _blue "经判断，现在内存加Swap和为${new_ms}Mi，满足GB5测试条件，测试开始。\n"
-            else
-                echo
-                echo "很抱歉，由于未知原因，Swap未能成功新增，现在内存加Swap和为${new_ms}Mi，仍不满足GB5测试条件，有如下备选方案："
-                echo "1. 强制执行GB5测试"
-                echo -e "2. 退出测试\n"
-                _yellow "请输入您的选择 (序号)：\c"
-                read -r choice_2
-                echo -e "\033[0m"
-                case "$choice_2" in
-                    2)
-                        swapoff ./GB5-test-32037e55c3/dd &> /dev/null
-                        rm -rf ./GB5-test-32037e55c3
-                        exit;;
-                    1)
-                        echo ;;
-                    *)
-                        rm -rf ./GB5-test-32037e55c3
-                        _red "输入错误，请重新执行脚本"
-                        exit;;
-                esac
-            fi;;
-        *)
-            rm -rf ./GB5-test-32037e55c3
-            _red "输入错误，请重新执行脚本"
-            exit;;
-    esac
-fi
-
-# 官方文件的SHA-256值
-GB5_official_sha256=32037e55c3dc8f360fe16b7fbb188d31387ea75980e48d8cf028330e3239c404
-
-# 下载GB5测试程序
-_yellow "GB5测试程序下载中 (该文件较大)"
-# 判断是否为境内IP
-country=$(curl ipinfo.io/country 2> /dev/null)
-if [ -z "$country" ] || echo "$country" | grep "{" > /dev/null
-then
-    echo "使用镜像源"
-    curl --progress-bar -o ./GB5-test-32037e55c3/Geekbench-5.5.1-Linux.tar.gz "$url_2"
-elif [ "$country" != "CN" ]
-then
-    echo "使用默认源"
-    curl --progress-bar -o ./GB5-test-32037e55c3/Geekbench-5.5.1-Linux.tar.gz "$url_1"
-else
-    echo "使用镜像源"
-    curl --progress-bar -o ./GB5-test-32037e55c3/Geekbench-5.5.1-Linux.tar.gz "$url_2"
-fi
-_blue "GB5测试程序下载完成\n"
-
-# 比对SHA-256
-_yellow "文件SHA-256比对中"
-GB5_download_sha256=$(sha256sum ./GB5-test-32037e55c3/Geekbench-5.5.1-Linux.tar.gz | awk '{print $1}')
-
-
-if [ "$GB5_download_sha256" == "$GB5_official_sha256" ]
-then
-    _blue "经比对，下载的程序与官网SHA-256相同，放心使用\n"
-else
-    _red "经比对，下载的程序与官网SHA-256不相同，退出脚本执行"
-    _red "事关重大，方便的话麻烦到 https://github.com/i-abc/gb5 提一个issue"
-    exit
-fi
-
-# 解压缩包
-_yellow "程序处理中"
-tar -xf ./GB5-test-32037e55c3/Geekbench-5.5.1-Linux.tar.gz -C ./GB5-test-32037e55c3
+_mkdir
 
 clear
-
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo -e '#            专用于服务器的GB5测试             #'
-echo -e '#                 '$GB5_version'                  #'
-echo -e '#        https://github.com/i-abc/gb5          #'
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo
-
-# 测试
-_yellow "测试中\n"
-echo "若出现 Warning: Geekbench may crash on systems with less than 1GB of RAM 提示无需理会，不影响测试"
-
-start_time=$(date +%s)
-
-./GB5-test-32037e55c3/Geekbench-5.5.1-Linux/geekbench_x86_64 | \
-    tee >(awk '/System Information/ {flag=1} flag && count<68 {print; count++}') > ./GB5-test-32037e55c3/gb5-output.txt
-
-end_time=$(date +%s)
-
-# 下载测试结果
-result_download_url=$(grep -E "https.*cpu\/[0-9]*$" ./GB5-test-32037e55c3/gb5-output.txt)
-if wget --spider $result_download_url 2> /dev/null
-then
-    wget -O ./GB5-test-32037e55c3/index.html $result_download_url 2> /dev/null
-else
-    wget --no-check-certificate  -O ./GB5-test-32037e55c3/index.html $result_download_url 2> /dev/null
-fi
-
-# 计算测试运行时间
-execution_time=$((end_time-start_time))
-execution_time_minutes=$((execution_time/60))
-execution_time_seconds=$((execution_time%60))
+_banner
+_swap
 
 clear
+_banner
+_judge_ip
+
+_download_geekbench
 
 echo
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
-echo -e '#            专用于服务器的GB5测试             #'
-echo -e '#                 '$GB5_version'                  #'
-echo -e '#        https://github.com/i-abc/gb5          #'
-echo -e '# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## #'
+_sha256
+
+_tar
+
+clear
+_banner
+
+_run
+
+_download_result_html
+
+clear
 echo
+_banner
 
-# 输出时间、分数、链接
-echo "当前时间: $(date +"%Y-%m-%d %H:%M:%S %Z")"
-echo -e "净测试时长: $execution_time_minutes分$execution_time_seconds秒\n"
-
-_yellow "Geekbench 5 测试结果\n"
-awk '/System Information/ {flag=1} flag && count<20 {sub("System Information", "系统信息"); sub("Processor Information", "处理器信息"); sub("Memory Information", "内存信息"); print; count++}' ./GB5-test-32037e55c3/gb5-output.txt
-awk -F'>' '/<div class='"'"'score'"'"'>/{print $2}' ./GB5-test-32037e55c3/index.html | awk -F'<' '{if (NR==1) {print "单核测试分数: "$1} else {print "多核测试分数: "$1}}'
-awk '/https.*cpu\/[0-9]*$/{print "详细结果链接: " $1}' ./GB5-test-32037e55c3/gb5-output.txt
+_output
 
 echo
-awk '/https.*key=[0-9]*$/{print "个人保存链接: " $1}' ./GB5-test-32037e55c3/gb5-output.txt
-
-# 删除残余文件
-echo
-swapoff ./GB5-test-32037e55c3/dd &> /dev/null
-rm -rf ./GB5-test-32037e55c3
-_yellow "残余文件清除成功"
+_delete
